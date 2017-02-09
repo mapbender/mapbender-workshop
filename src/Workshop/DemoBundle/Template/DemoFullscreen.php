2529fd7d8ae765c87c82673eb4da16cd7feb82e1
Astrid Emde <astrid.emde@wheregroup.com>
2017-02-09 19:36:09 +0100

    only define title, css & twig and extend Fullscreen Template

--- src/Workshop/DemoBundle/Template/DemoFullscreen.php
+++ src/Workshop/DemoBundle/Template/DemoFullscreen.php
@@ -1,111 +1,22 @@
 <?php
+
 namespace Workshop\DemoBundle\Template;
 
-use Mapbender\CoreBundle\Component\Template;
+use Mapbender\CoreBundle\Template\Fullscreen;
 
 /**
  * Template DemoFullscreen
  * 
  * @author Astrid Emde
  */
-class DemoFullscreen extends Template
+class DemoFullscreen extends Fullscreen
 {
-    /**
-     * @inheritdoc
-     */
-    public static function getRegionsProperties()
-    {
-        return array(
-            'sidepane' => array(
-                'tabs' => array(
-                    'name' => 'tabs',
-                    'label' => 'mb.manager.template.region.tabs.label'),
-                'accordion' => array(
-                    'name' => 'accordion',
-                    'label' => 'mb.manager.template.region.accordion.label')
-            )
-        );
-    }
+    protected static $title = 'Workshop Demo Fullscreen Template (Kreis Groß-Gerau)';
+    protected static $css   = array(
+        '@MapbenderCoreBundle/Resources/public/sass/template/fullscreen.scss',
+        '@WorkshopDemoBundle/Resources/public/demo_fullscreen.css',
+    );
 
-    /**
-     * @inheritdoc
-     */
-    public static function getTitle()
-    {
-        return 'DemoFullscreen';
-    }
-
-    /**
-     * @inheritdoc
-     */
-    static public function listAssets()
-    {
-        $assets = array(
-            'css' => array('@MapbenderCoreBundle/Resources/public/sass/template/fullscreen.scss','@WorkshopDemoBundle/Resources/public/demo_fullscreen.css'),
-            'js'    => array(
-                '/components/underscore/underscore-min.js',
-                '@FOMCoreBundle/Resources/public/js/widgets/popup.js',
-                '@FOMCoreBundle/Resources/public/js/frontend/sidepane.js',
-                '@FOMCoreBundle/Resources/public/js/frontend/tabcontainer.js',
-                '@MapbenderCoreBundle/Resources/public/regional/vendor/notify.0.3.2.min.js',
-                "/components/datatables/media/js/jquery.dataTables.min.js",
-                '/components/jquerydialogextendjs/jquerydialogextendjs-built.js',
-                "/components/vis-ui.js/vis-ui.js-built.js"
-
-            ),
-            'trans' => array()
-        );
-        return $assets;
-    }
-    
-    /**
-     * @inheritdoc
-     */
-    public function getLateAssets($type)
-    {
-        $assets = array(
-            'css' => array(),
-            'js' => array(),
-            'trans' => array()
-        );
-        return $assets[$type];
-    }    
-
-    /**
-     * @inheritdoc
-     */
-    public function getAssets($type)
-    {
-        $assets = $this::listAssets();
-        return $assets[$type];
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public static function getRegions()
-    {
-        return array('toolbar', 'sidepane', 'content', 'footer');
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function render($format = 'html', $html = true, $css = true,$js = true)
-    {
-        $region_props = $this->application->getEntity()->getNamedRegionProperties();
-        $default_region_props = $this->getRegionsProperties();
-
-        $templating = $this->container->get('templating');
-        return $templating
-                ->render('WorkshopDemoBundle:Template:demo_fullscreen.html.twig',
-                    array(
-                    'html' => $html,
-                    'css' => $css,
-                    'js' => $js,
-                    'application' => $this->application,
-                    'region_props' => $region_props,
-                    'default_region_props' => $default_region_props));
-    }
+    public $twigTemplate = 'WorkshopDemoBundle:Template:demo_fullscreen.html.twig';
 
 }
